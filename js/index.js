2b876f4efe478b98664399702433532fd0912a35
Mr-Wang <66669531+Mr-WWT@users.noreply.github.com>
2020-09-08 16:03:54 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,28 +1,25 @@
 $(function () {
-    //纯手打
-    //进入缓冲
     $('#preloader').delay(550).fadeOut('slow');
-
-    //导航效果
+    
     $(window).scroll(function () {
-        if ($(document).scrollTop() >= 550) {
-            $(".nav").stop().fadeIn(100)
+        var introH = $(".banner").height() - 90
+        if ($(document).scrollTop() > introH) {
+            $(".nav").fadeIn(400)
 
         } else {
-            $(".nav").stop().fadeOut(100)
+            $(".nav").fadeOut(200)
             $(".nav-toggle").removeClass("active")
             $(".nav-menu").removeClass("active")
         };
-        console.log($(document).scrollTop());
+        
     });
 
-    //响应式导航效果未完善
+
     $(".nav-toggle").click(function () {
         $(this).toggleClass("active");
         $(this).siblings(".nav-menu").toggleClass("active")
     });
 
-    //打字效果
     var text_typed = $(".text").text();
     $(".text1").typed({
         strings: text_typed.split(","),
@@ -32,7 +29,6 @@ $(function () {
         backSpeed: 50
     });
 
-    //li索引到指定div
     $(".nav-menu li,.down").click(function () {
         var index = $(this).index();
         var top = $(".main3 .w").eq($(this).index()).offset().top;
@@ -42,7 +38,6 @@ $(function () {
 
     });
 
-    //li到指定位置::before显示
     $(window).scroll(function () {
         toggleTool();
         $(".main3 .w").each(function (i, ele) {
@@ -53,9 +48,9 @@ $(function () {
         });
     });
 
-    //返回顶部按钮指定位置淡出淡入
     var top = $(".main3-1").offset().top;
     toggleTool();
+
     function toggleTool() {
         if ($(document).scrollTop() >= top) {
             $(".a-top").fadeIn();
@@ -63,7 +58,7 @@ $(function () {
             $(".a-top").fadeOut();
         };
     };
-    //返回顶部
+
     $(".a-top,.logo").click(function () {
         $("html,body").stop().animate({
             scrollTop: 0
